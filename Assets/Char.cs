7e2f3572d3b93f31bd9c2f330e2a9a71e51b0e7b
NDXDirectorsCut <49319841+NDXDirectorsCut@users.noreply.github.com>
2022-02-21 20:17:40 +0200

    LookTo Mouse

--- Assets/Char.cs
+++ Assets/Char.cs
@@ -6,14 +6,16 @@ public class Char : MonoBehaviour
 {
 	public float MvS = 15f;
 	public Rigidbody2D Pl;	
-	
 	Vector2 MvM;	
+
 
 	void Update()
     {
 	//Input Handler
 	float mY = Input.GetAxis ("Mouse Y");
 	float mX = Input.GetAxis ("Mouse X");
+	Vector3 mP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	transform.rotation = Quaternion.LookRotation(Vector3.forward, mP - transform.position);
 	MvM.x = Input.GetAxisRaw("Horizontal");
 	MvM.y = Input.GetAxisRaw("Vertical");
     }
